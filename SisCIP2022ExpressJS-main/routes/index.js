7bc27b3e329f1fb6275e87e21d55090f5d165cbc
Guino-Elvis <ginoyujra38@gmail.com>
2022-07-09 22:25:39 -0500

    avanzee colegiado

--- SisCIP2022ExpressJS-main/routes/index.js
+++ SisCIP2022ExpressJS-main/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var dbConn  = require('../lib/db');
 
-/* GET home page 1. */
+/* GET home page 1. 
 router.get('/', function(req, res, next) {
   res.render('indexG', { title: 'Express' });
 });
@@ -18,13 +18,63 @@ router.get('/registroexitosoG', function(req, res, next) {
   res.render('registroexitosoG');
 });
 
+router.post('/mainmodeG', function(req, res, next) {
+  email=req.body.email;
+  password=req.body.password;
+  console.log(email);
+  dbConn.query("SELECT * FROM usuarios WHERE email='"+email+"' AND password='"+password+"'",function(err,rows){
+    if(err){
+      req.flash('error',err);
+      console.log(err);
+    }else{
+      if(rows.length){
+        console.log(rows);
+        req.session.idu=rows[0]["id"];
+        req.session.email=rows[0]["email"];
+        req.session.loggedin = true;
+        res.redirect('/main');
+      }else{
+        req.flash('error','El usuario no existe...');
+        res.redirect('/loginG');
+      }
+    }
+  });
+
+  
+});
+
+router.get('/main', function(req, res, next) {
+  //if(!req.session.loggedin){
+  //  res.redirect('/login');
+  //}else{
+    res.locals.idu=req.session.idu;
+    res.locals.email=req.session.email;
+    res.locals.loggedin=req.session.loggedin;
+
+    var queries = [
+     "SELECT COUNT(idx) as cantidad FROM clientes",
+     "SELECT SUM(saldo) as total FROM clientes"
+   ];
+   
+    //dbConn.query('SELECT SUM(saldo) as total FROM clientes',function(err,rows) {
+    dbConn.query(queries.join(';'),function(err,rows) {
+     //console.log(rows[0].total);
+     if(err) throw err;
+     //console.log(rows[0][0].cantidad);
+     res.render('mainmodeG',{dataCantidad:rows[0][0].cantidad,dataSaldo:rows[1][0].total});
+    });
+    
+  //}
+});
+
+
 
 
 router.get('/mainmodeG', function(req, res, next) {
   res.render('mainmodeG');
 });
 
-module.exports = router;
+module.exports = router; */
 
 
 
@@ -41,14 +91,16 @@ module.exports = router;
 
 
 
-/* GET home page 2. 
+/* GET home page 2. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-*/
+
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+
 
 router.post('/dashboard', function(req, res, next) {
   email=req.body.email;
